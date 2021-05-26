95f394f3029912e94ad74d3b7637c228eb29858e
andjstam <andjstam@elfak.rs>
2021-05-26 23:35:46 +0200

    first commit

---
+++ back/DirectorgBaze/Models/LoggedUser.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DirectorgBaze.Models
+{
+    public class LoggedUser
+    {
+        public ObjectId Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+    }
+}
